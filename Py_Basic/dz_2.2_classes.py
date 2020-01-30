d61c172a20e1abd403befc2e6bbb1985287734e6
Aleksey Lapshin <lapssh@yandex.ru>
2020-01-30 23:19:28 +0300

    DZ_EDO - final

---
+++ Py_Basic/dz_2.2_classes.py
@@ -0,0 +1,11 @@
+class Animal:
+
+    def __init__(self):
+        self.name = name
+    def feed(self):
+        """
+        Кормим  животинку
+        :return:
+        """
+        print('Животинка радуется...')
+
