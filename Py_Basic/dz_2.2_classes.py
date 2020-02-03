21f4c0594e5537fe411c0b62d7a9708569b7b15f
Aleksey Lapshin <lapssh@yandex.ru>
2020-02-03 17:39:38 +0300

    Class Farm Work in progress

--- Py_Basic/dz_2.2_classes.py
+++ Py_Basic/dz_2.2_classes.py
@@ -1,7 +1,8 @@
 import random
 class Animal:
-    def __init__(self, name):
+    def __init__(self, name, weight):
         self.name = name
+        self.weight = weight
     def feed(self):
         """
         Кормим  животинку
@@ -20,8 +21,20 @@ class Chicken(Goose):
     def get_eggs(self):
         return random.randint(0,10)
 
+class Cow(Animal):
+    yield_of_milk = 90
+    def get_milk(self):
+        return random.randint(yield_of_milk/3, yield_of_milk)
 
-goose_white = Goose('Белый')
-goose_grey = Goose('Серый')
+
+goose_white = Goose('Белый', 3)
+goose_grey = Goose('Серый', 3)
+chicken01 = Chicken('Ко-Ко', 1.4)
+chicken02 = Chicken('Кукареку', 1.9)
+cow01 = Cow('Манька', 500)
+
 print(goose_white.name)
 print('Сегодня Серый снес яйца в колличестве: ', goose_grey.get_eggs())
+
+print('А сколько корова дает молока?')
+print(cow01.get_milk())
