37d382f9df3768e62831e95c7df9141c22583330
lapssh <lapssh@yandex.ru>
2020-02-23 17:44:56 +0300

    Косметические правки

--- Py_Basic/dz_3_2_requests_example.py
+++ Py_Basic/dz_3_2_requests_example.py
@@ -1,8 +1,7 @@
 import requests
 import os
 import OAuth_TOKEN
-from pprint import pprint
-#  API диска  с полигона AgAAAAAjPcXKAADLW2Vd5_LN5kb3qIhAlLj4hcc
+
 #  документация https://yandex.ru/dev/translate/doc/dg/reference/translate-docpage/
 
 API_KEY = 'trnsl.1.1.20190712T081241Z.0309348472c8719d.0efdbc7ba1c507292080e3fbffe4427f7ce9a9f0'
