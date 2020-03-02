efa5a00c9ce462bd0bccbd46c8eb7bb2aec9ec44
lapssh <lapssh@yandex.ru>
2020-03-03 00:11:47 +0300

    added DZ PYDA-8 datetime

--- Py_Data/test.py
+++ Py_Data/test.py
@@ -1,47 +1,30 @@
-stream = [
-    '2018-01-01,user1,3',
-    '2018-01-07,user1,4',
-    '2018-03-29,user1,1',
-    '2018-04-04,user1,13',
-    '2018-01-05,user2,7',
-    '2018-06-14,user3,4',
-    '2018-07-02,user3,10',
-    '2018-03-21,user4,19',
-    '2018-03-22,user4,4',
-    '2018-04-22,user4,8',
-    '2018-05-03,user4,9',
-    '2018-05-11,user4,11',
-]
-uniq_user_count = 0
-view_count = 0
-user_name = ''
-for i in stream:
-    s = i.split(',')
-    view_count += int(s[2])
-    if user_name != s[1]:
-        uniq_user_count += 1
-        user_name = s[1]
-print(uniq_user_count, view_count,  view_count / uniq_user_count)
+#Задание 4 (бонусное)
+#Ваш коллега прислал код функции:
 
-# avg_user_view = {}
-# count_view = 0
-# for i in stream:
-#     s = i.split(',')
-#     if s[1] not in avg_user_view:
-#         avg_user_view[s[1]] = int(s[2])
-#         count_view += 1
-#         continue
-#     avg_user_view[s[1]] += int(s[2])
-#     count_view += 1
-# print(avg_user_view)
+DEFAULT_USER_COUNT = 3
 
-#     while i[2]
-# count_uniq = dict()
-# for user in list_stream:
-#     if user[1] not in count_uniq:
-#         count_uniq[user[1]] = [int(user[2]), 1]
-#     else:
-#         count_uniq[user[1]][0] += int(user[2])
-#         count_uniq[user[1]][1] += 1
-# for user, value in count_uniq.items():
-#     print(f'У пользователя {user} было в среднем {value[0]/value[1]} просмотров.')
+def delete_and_return_last_user(region, default_list=['A100', 'A101', 'A102']):
+    """
+        Удаляет из списка default_list последнего пользователя
+и возвращает ID нового последнего пользователя.
+    :param region:
+    :param default_list:
+    :return:
+    """
+    element_to_delete = default_list[-1]
+    default_list.remove(element_to_delete)
+
+    return default_list[DEFAULT_USER_COUNT-2]
+#При однократном вызове этой функции все работает корректно:
+print(delete_and_return_last_user(1))
+print(delete_and_return_last_user(1))
+
+
+#'A101'
+
+# Однако, при повторном вызове получается ошибка IndexError: list index out of range.
+#
+# Задание:
+#
+# Что значит ошибка list index out of range?
+# Почему при первом запуске функция работает корректно, а при втором - нет?
