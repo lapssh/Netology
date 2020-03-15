bc39e8038b84a15a437674272a1593f97dfbf1ee
lapssh <lapssh@yandex.ru>
2020-03-15 21:43:48 +0300

    added dz_regex.ipynb (PyDA-8)

--- Py_Data/test.py
+++ Py_Data/test.py
@@ -1,30 +1,8 @@
-#Задание 4 (бонусное)
-#Ваш коллега прислал код функции:
-
-DEFAULT_USER_COUNT = 3
-
-def delete_and_return_last_user(region, default_list=['A100', 'A101', 'A102']):
-    """
-        Удаляет из списка default_list последнего пользователя
-и возвращает ID нового последнего пользователя.
-    :param region:
-    :param default_list:
-    :return:
-    """
-    element_to_delete = default_list[-1]
-    default_list.remove(element_to_delete)
-
-    return default_list[DEFAULT_USER_COUNT-2]
-#При однократном вызове этой функции все работает корректно:
-print(delete_and_return_last_user(1))
-print(delete_and_return_last_user(1))
-
-
-#'A101'
-
-# Однако, при повторном вызове получается ошибка IndexError: list index out of range.
-#
-# Задание:
-#
-# Что значит ошибка list index out of range?
-# Почему при первом запуске функция работает корректно, а при втором - нет?
+import re
+pattern = r'[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+.[a-zA-Z.]{2,64}$'
+test_mails = ['admin@gmail.com', 'support12@yandex.ru', 'MaMa@@family.io.oi', '12admin@12mail.ru']
+for i in test_mails:
+    if re.match(pattern, i):
+        print(i, '- соответствует')
+    else:
+        print(i, '- не соответствует')
