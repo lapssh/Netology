7c2d735270f9dc75f580ace453ad76b5070a6ebc
lapssh <lapssh@yandex.ru>
2020-03-27 10:39:24 +0300

    dz_02_1 complite TrueIterator   (adpy-11)

--- adpy-11/hw_adpy_02/main.py
+++ adpy-11/hw_adpy_02/main.py
@@ -1,25 +1,34 @@
 import requests, json
+
 WIKI_LINK = 'https://en.wikipedia.org/wiki/'
 
+
 class TrueIterator:
-    def __init__(self):
-        pass
+    def __init__(self, data):
+        self.end = len(data)
+        self.index = 0
+        self.data = data
+
     def __iter__(self):
         return self
+
     def __next__(self):
-        pass
+        self.index += 1
+        if self.index == self.end:
+            raise StopIteration
+        return self.data[self.index]
+
 
 if __name__ == '__main__':
     with open('countries.json') as f:
         data = json.load(f)
-        for country in data:
+        for country in TrueIterator(data):
             print(country['name']['official'])
             country_link = WIKI_LINK + country['name']['official']
             response = requests.get(country_link)
-            #print(type(response.status_code))
             if response.status_code != 200:
                 with open('country-wiki.txt', 'at', encoding='utf-8') as f2:
-                    f2.write(country['name']['official'] + '\t-\t На википедии такой страны не найдено\n' )
+                    f2.write(country['name']['official'] + '\t-\t На википедии такой страны не найдено\n')
             else:
                 with open('country-wiki.txt', 'at', encoding='utf-8') as f2:
                     f2.write(country['name']['official'] + '\t-\t' + country_link + '\n')
