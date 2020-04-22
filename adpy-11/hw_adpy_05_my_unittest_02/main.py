c4df289c7b702b641cf528165bc76ef8dabe4e0c
lapssh <lapssh@yandex.ru>
2020-04-22 10:57:40 +0300

    homework unit_test task 2 complite  (adpy-11)

--- adpy-11/hw_adpy_05_my_unittest_02/main.py
+++ adpy-11/hw_adpy_05_my_unittest_02/main.py
@@ -2,41 +2,42 @@ import OAUTH_TOKEN
 import requests
 import json
 
-TOKEN = OAUTH_TOKEN.OAUTH_TOKEN
-API_KEY = OAUTH_TOKEN.API_KEY
-URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
+class TranslateMe:
 
-def translate_it(text, from_lang, to_lang='ru'):
-    """
-    https://translate.yandex.net/api/v1.5/tr.json/translate ?
-    key=<API-ключ>
-     & text=<переводимый текст>
-     & lang=<направление перевода>
-     & [format=<формат текста>]
-     & [options=<опции перевода>]
-     & [callback=<имя callback-функции>]
-    :param to_lang:
-    :return:
-    """
+    def __init__(self, name, text, from_lang='en', to_lang='ru'):
+        self.name = name
+        self.text = text
+        self.from_lang = from_lang
+        self.to_lang = to_lang
+        self.TOKEN = OAUTH_TOKEN.OAUTH_TOKEN
+        self.API_KEY = OAUTH_TOKEN.API_KEY
+        self.URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+        self.params = {
+            'key': self.API_KEY,
+            'text': self.text,
+            'lang': self.from_lang + '-' + self.to_lang
+        }
 
+    def translate_it(self):
+        """
+        https://translate.yandex.net/api/v1.5/tr.json/translate ?
+        key=<API-ключ>
+         & text=<переводимый текст>
+         & lang=<направление перевода>
+         & [format=<формат текста>]
+         & [options=<опции перевода>]
+         & [callback=<имя callback-функции>]
+        :param to_lang:
+        :return:
+        """
+        self.response = requests.get(self.URL, params=self.params)
+        self.json_ = self.response.json()
+        return self.json_
 
-    params = {
-        'key': API_KEY,
-        'text': text,
-        'lang': from_lang + '-' + to_lang
-    }
-
-    response = requests.get(URL, params=params)
-    json_ = response.json()
-    return json_
 
 if __name__ == '__main__':
-    from_lang = 'en'
-    json_response = translate_it('hello', from_lang)
-    print('Исходное слово: hello. Перевод: ',json_response['text'][0])
-    print('Переведено сервисом «Яндекс.Переводчик» http://translate.yandex.ru.')
-    print(json_response)
-    print(json_response['code'])
-    print(json_response['text'][0])
-
+    zapros01 = TranslateMe('zapros01', 'hello', to_lang='de')
+    print(zapros01.translate_it())
+    zapros02 = TranslateMe('zapros02', 'Di katze', from_lang='de')
+    print(zapros02.translate_it())
