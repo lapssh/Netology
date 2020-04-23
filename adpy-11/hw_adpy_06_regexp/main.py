a2e615b3f88aa3c1c70306d3416381ee1a26358b
lapssh <lapssh@yandex.ru>
2020-04-23 12:15:22 +0300

    homework regex complite (adpy-11)

--- adpy-11/hw_adpy_06_regexp/main.py
+++ adpy-11/hw_adpy_06_regexp/main.py
@@ -13,63 +13,44 @@ def import_data(file='phonebook_raw.csv'):
         contacts_list = list(rows)
         return contacts_list
 
+
 def merge_duplicate(base_humans):
     """Удаляем дубликаты"""
 
     base_humans2 = []
-    while len(base_humans) >= 2:
+    while base_humans != []:
         tmp_user = base_humans[0]
-        print('tmp = ', tmp_user.lastname)
         base_humans.pop(0)
-        print('вырезал первого')
         for user in base_humans:
-            print(tmp_user.lastname, ' равен ли ', user.lastname)
-            if tmp_user.lastname == user.lastname:
-                print('парняги говорят что да')
-                tmp_user.firstname = user.firstname
-                tmp_user.surname = user.surname
-                tmp_user.company = user.company
-                tmp_user.position = user.position
-                tmp_user.tel_number = user.tel_number
-                tmp_user.email = user.email
-                for i in base_humans2:
-                    if tmp_user.lastname == i.lastname:
-                        pass
-                    else:
-                        base_humans2.append(tmp_user)
-
-                # print('парняги говорят - НЕТ')
-                # base_humans2.append(user)
+            if tmp_user.lastname == user.lastname and tmp_user.firstname == user.firstname:
+                if tmp_user.surname == '':
+                    tmp_user.surname = user.surname
+                if tmp_user.company == '':
+                    tmp_user.company = user.company
+                if tmp_user.position == '':
+                    tmp_user.position = user.position
+                if tmp_user.tel_number == '':
+                    tmp_user.tel_number = user.tel_number
+                if tmp_user.email == '':
+                    tmp_user.email = user.email
+        else:
+            for i in base_humans2:
+                if i.lastname == tmp_user.lastname:
+                    break
+            else:
+                base_humans2.append(tmp_user)
     return base_humans2
 
 
-    # for id, user in enumerate(base_humans):
-    #     for user2 in enumerate(base_humans)+1:
-    #         if user.lastname == user2.lastname and user.firstname == user2.firstname:
-    #             print('Найден дубль для ', user.lastname, user.firstname)
-    #             print(user.tel_number, user2.tel_number)
-    #             if user.tel_number == '':
-    #                 user.tel_number = user2.tel_number
-    #             if user.company == '':
-    #                 user.company = user2.company
-    #                 print(user.company, 'добавили!')
-    #             if user.position == '':
-    #                 user.position = user2.position
-    #                 print('добавил должность', user.position)
-    #             if user.email == '':
-    #                 user.email = user2.email
-    #             if user.surname == '':
-    #                 try:
-    #                     user.surname = user2.surname
-    #                 except:
-    #                     pass # отчество отсутствует в обоих запясях
-    #             base_humans_without_double.append(user)
-    #         user2_index += 1
-    # return base_humans_without_double
-
-
-
-
+def make_new_contact_list(base_humans):
+    new_contact_list = []
+    new_contact_list.append('lastname,firstname,surname,organization,position,phone,email\n')
+    for i in base_humans:
+        row = i.lastname + ',' + i.firstname + ',' + i.surname + ',' + i.company + ',' + (
+                i.position + ',' + i.tel_number + ',' + i.email + '\n'
+        )
+        new_contact_list.append(row)
+    return new_contact_list
 
 
 if __name__ == '__main__':
@@ -78,13 +59,9 @@ if __name__ == '__main__':
     contacts_list = import_data()
 
 
-    # pprint(contacts_list)
-
     class Human:
         def __init__(self, data):
             self.data = data
-
-
 
         def find_email(self):
             self.email = self.data[6]
@@ -95,7 +72,7 @@ if __name__ == '__main__':
             pattern_1 = re.compile(r'(\+7|8|7).*?(\d{3}).*?(\d{3}).*?(\d{2}).*?(\d{2})')
             pattern_2 = re.compile(r'(\+7|8|7).*?(\d{3}).*?(\d{3}).*?(\d{2}).*?(\d{2}).*?(\d{4)')
             self.tel_number = re.sub(pattern_1, '+7(\\2)\\3-\\4-\\5', self.tel_number)
-            self.tel_number = re.sub(pattern_2,'+7(\\2)\\3-\\4-\\5 доб.\\6', self.tel_number)
+            self.tel_number = re.sub(pattern_2, '+7(\\2)\\3-\\4-\\5 доб.\\6', self.tel_number)
             return self.tel_number
 
         def find_position(self):
@@ -107,7 +84,7 @@ if __name__ == '__main__':
             return self.company
 
         def find_name(self):
-            self.full_name = self.data[0] +' '+ self.data[1] + ' ' + self.data[2]
+            self.full_name = self.data[0] + ' ' + self.data[1] + ' ' + self.data[2]
             self.full_name = self.full_name.split()
             self.lastname = self.full_name[0]
             try:
@@ -115,29 +92,22 @@ if __name__ == '__main__':
                 self.surname = self.full_name[2]
             except:
                 self.surname = ''
-                # если отсутствует отчество
-            # if len(self.full_name) == 2:
-            #     self.firstname = self.full_name[1]
-            #     print('длинна равна имени для ', self.firstname)
-            #     if len(self.full_name) == 3:
-            #         self.surname = self.full_name[2]
             return self.full_name
 
         def show_base(self):
             try:
                 print('-' * 40)
-                print('Фамилия: \t',self.lastname,'\nИмя: \t\t', self.firstname, '\nОтчество: \t', self.surname)
+                print('Фамилия: \t', self.lastname, '\nИмя: \t\t', self.firstname, '\nОтчество: \t', self.surname)
                 print('Организация:', self.company)
                 print('Должность:\t', self.position)
                 print('Телефон:\t', self.tel_number)
                 print('e-mail:\t\t', self.email)
             except:
-                print('Фамилия: \t',self.lastname,'\nИмя: \t\t', self.firstname) # отсутствует отчество
+                print('Фамилия: \t', self.lastname, '\nИмя: \t\t', self.firstname)  # отсутствует отчество
                 print('Организация:', self.company)
                 print('Должность:\t', self.position)
                 print('Телефон:\t', self.tel_number)
                 print('e-mail:\t\t', self.email)
-
 
 
     base_humans = list()
@@ -150,22 +120,18 @@ if __name__ == '__main__':
         i.find_company()
         i.find_name()
 
-    for i in base_humans:
-        i.show_base()
-
-    #base_humans_without_doubles = merge_duplicate(base_humans)
-    base_humans.pop(0)
-    print()
-    print()
+    base_humans.pop(0)  # отекаем заголовок
     print('БЕЗ ДУБЛИКАТОВ')
     base_humans2 = merge_duplicate(base_humans)
     for i in base_humans2:
-         i.show_base()
+        i.show_base()
 
+    new_contact_list = make_new_contact_list(base_humans2)
+    pprint(contacts_list)
+    print()
+    pprint(new_contact_list)
 
     # TODO 2: сохраните получившиеся данные в другой файл
     # код для записи файла в формате CSV
     with open("phonebook.csv", "w", encoding='utf-8') as f:
-        datawriter = csv.writer(f, delimiter=',')
-        # Вместо contacts_list подставьте свой список
-        datawriter.writerows(contacts_list)
+        f.writelines(new_contact_list)
